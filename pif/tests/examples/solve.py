df0d688aa49b9a9da54b14b4c77f9525276a9095
mario <mariofares02@gmail.com>
2024-05-05 19:31:32 -0400

    Setting it up as a package.

---
+++ pif/tests/examples/solve.py
@@ -0,0 +1,4 @@
+from pif.src.pif.types.secure_num import SecureNum
+
+# Check basic functionality
+print(repr(SecureNum(5, 3)))
--- test/examples/solve.py
+++
@@ -1,5 +0,0 @@
-from pif.types.secure_num import SecureNum
-
-# Check basic functionality: assignment and repr
-a = SecureNum(5, 3)
-print(repr(a))
