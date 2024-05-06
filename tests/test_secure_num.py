f5e89543bdb4070f9acef29958cc828e425efca4
mario <mariofares02@gmail.com>
2024-05-06 13:23:27 -0400

    Tests initially complete.

--- tests/test_secure_num.py
+++ tests/test_secure_num.py
@@ -30,6 +30,75 @@ def test_pow():
     assert_val_level(b ** a, 2, 0)
 
 
+# Right operands
+def test_radd():
+    temp = 3 + a
+
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 4, 0)
+
+
+def test_rsub():
+    temp = 1 - a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 0, 0)
+
+
+def test_rmul():
+    temp = 1 * a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 1, 0)
+
+
+def test_rfloordiv():
+    temp = 1 // a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 1, 0)
+
+
+def test_rmod():
+    temp = 1 % a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 0, 0)
+
+
+def test_rpow():
+    temp = 1 ** a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 1, 0)
+
+
+def test_rand():
+    temp = 1 & a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 1, 0)
+
+
+def test_ror():
+    temp = 1 | a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 1, 0)
+
+
+def test_rxor():
+    temp = 1 ^ a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 0, 0)
+
+
+def test_rrshift():
+    temp = 1 >> a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 0, 0)
+
+
+def test_rlshift():
+    temp = 1 << a
+    assert isinstance(temp, SecureNum)
+    assert_val_level(temp, 2, 0)
+
+
+# Bitwise operators
 def test_and():
     assert_val_level(b & a, 0, 0)
 
@@ -50,6 +119,7 @@ def test_lshift():
     assert_val_level(b << a, 4, 0)
 
 
+# Comparison operators
 def test_lt():
     assert_val_level(a < b, True, 0)
 
@@ -80,7 +150,7 @@ def test_str():
 
 
 def test_repr():
-    assert a.__repr__() == f"<SecureNum: val: {a.get_value()}, sec: {a.get_level()}>"
+    assert a.__repr__() == f"<SecureNum: val: 1, sec: 0>"
 
 
 def test_get_value():
@@ -125,6 +195,29 @@ def test_set_level_neg():
 def test_set_level_str():
     try:
         a.set_level("s")
+    except Exception as e:
+        assert isinstance(e, ValueError)
+        return
+
+    assert False
+
+
+def test_wrapper_a():
+    temp = SecureNum.wrapper(a)
+    assert temp is a
+
+
+def test_wrapper_other():
+    temp = SecureNum.wrapper(3)
+
+    assert isinstance(temp, SecureNum)
+    assert temp.get_value() == 3
+    assert temp.get_level() == 0
+
+
+def test_wrapper_str():
+    try:
+        _ = SecureNum.wrapper("1")
     except Exception as e:
         assert isinstance(e, ValueError)
         return
